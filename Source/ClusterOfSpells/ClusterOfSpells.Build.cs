c4078a629d30e2c1aab4bf124abd391dfd226e43
pk6x <amgundam78@gmail.com>
2025-02-18 07:36:08 +0000

    Updated

--- Source/ClusterOfSpells/ClusterOfSpells.Build.cs
+++ Source/ClusterOfSpells/ClusterOfSpells.Build.cs
@@ -8,7 +8,8 @@ public class ClusterOfSpells : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", 
+			"CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
--- Source/ClusterOfSpells/ClusterOfSpells.cpp
+++ Source/ClusterOfSpells/ClusterOfSpells.cpp
@@ -3,4 +3,4 @@
 #include "ClusterOfSpells.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ClusterOfSpells, "ClusterOfSpells" );
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, ClusterOfSpells, "ClusterOfSpells");
